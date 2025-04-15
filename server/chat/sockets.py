6f46e2017f9050b29af6ba65dc31bf8bee435e9c
Isaac Silva <isaackun98@gmail.com>
2025-04-15 14:43:54 +0900

    feat: add initial socket event handlers structure

--- server/chat/sockets.py
+++ server/chat/sockets.py
@@ -1,5 +1,14 @@
-def socketio_events(socketio):
-    @socketio.on("message")
-    def handle_message(data):
-        print("Received:", data)
-        socketio.emit("message", data, broadcast=True)
+from flask_socketio import SocketIO, emit
+from db.mongo import messages_collection
+
+socketio = SocketIO(cors_allowed_origins="*")
+
+@socketio.on("connect")
+def handle_connect():
+    print("A user connected")
+
+@socketio.on("message")
+def handle_message(data):
+    print("Received message:", data)
+    messages_collection.insert_one({"text": data})
+    emit("message", data, broadcast=True)
